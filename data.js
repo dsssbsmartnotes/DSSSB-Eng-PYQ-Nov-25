2730a3d07d506735c3f6141650730ded7935b39a
dsssbsmartnotes <nishmit901@gmail.com>
2026-02-06 22:03:27 +0530

    Update data.js

--- data.js
+++ data.js
@@ -14,84 +14,84 @@ const testDatabase = [
         "category": "hindi",
         "title": "DSSSB HINDI PYQ_25_26",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/Hset01.html",
-        "attempts": "0"
+        "attempts": "2000"
     },
     {
         "id": "t_1770395168586",
         "category": "english",
         "title": "DSSSB_ENG_PYQ_25/26",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/english.html",
-        "attempts": "0"
+        "attempts": "3000"
     },
     {
         "id": "t_1770395073747",
         "category": "maths",
         "title": "DSSSB Maths PYQ – CBT (Nov 25)",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/maths-pyq-nov25.html",
-        "attempts": "0"
+        "attempts": "4500"
     },
     {
         "id": "t_1770395029747",
         "category": "english",
         "title": "DSSSB ENG PYQ_2025_26",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/english.html",
-        "attempts": "0"
+        "attempts": "6000"
     },
     {
         "id": "t_1770394952169",
         "category": "gk",
         "title": "DSSSB GK PYQ_2025_26",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/DSSSB%20gk%20pyq.html",
-        "attempts": "0"
+        "attempts": "3500"
     },
     {
         "id": "t_1770394900065",
         "category": "english",
         "title": "DSSSB ENG PYQ",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/eng01-10-25p12ndsft.html",
-        "attempts": "0"
+        "attempts": "1122"
     },
     {
         "id": "t_1770394819691",
         "category": "maths",
         "title": "MATH PYQ DASS_IV",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/Math.html",
-        "attempts": "0"
+        "attempts": "4600"
     },
     {
         "id": "t_1770394673449",
         "category": "english",
         "title": "ENGLISH PYQ TEST – DASS-IV (03 Nov | 2nd Shift)",
         "url": "https://dsssbsmartnotes.github.io/dsssb-mts-cbt/eng-pyq-01.html",
-        "attempts": "0"
+        "attempts": "1300"
     },
     {
         "id": "t_1770394549838",
         "category": "english",
         "title": "CGL Mains English section(2026)",
         "url": "https://dsssbsmartnotes.github.io/Dsssb-Tests/cglengmains25.html",
-        "attempts": "0"
+        "attempts": "1103"
     },
     {
         "id": "t_1770391851913",
         "category": "english",
         "title": "Warder_pyq_01",
         "url": "https://dsssbsmartnotes.github.io/DSSSB-Eng-PYQ-Nov-25/warderengpyq.html",
-        "attempts": "0"
+        "attempts": "100"
     },
     {
         "id": "mts_01",
         "category": "mts-full",
         "title": "MTS Full Length Mock Test - 01",
         "url": "https://forms.gle/YOUR_GOOGLE_FORM_LINK_HERE",
-        "attempts": "15.2k"
+        "attempts": "0"
     },
     {
         "id": "as_01",
         "category": "asst-supt",
         "title": "Asst. Supt. Full Mock (pyq based)",
         "url": "https://google.com",
-        "attempts": "10.1k"
+        "attempts": "0"
     },
     {
         "id": "eng_02",
